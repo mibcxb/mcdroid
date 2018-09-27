05d14f95b04c388c6d9a3d17af7d4f4b05f0b4f9
chenxiaobo <chenxiaobo.xian@haier.com>
2018-09-27 21:33:23 +0800

    升级compileSdkVersion到28

--- McDroid/mcdroid/src/main/java/com/mibcxb/droid/util/RxJavaUtils.java
+++ McDroid/mcdroid/src/main/java/com/mibcxb/droid/util/RxJavaUtils.java
@@ -3,11 +3,19 @@ package com.mibcxb.droid.util;
 import android.support.annotation.NonNull;
 
 import io.reactivex.Observable;
+import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 public class RxJavaUtils {
     public static <T> Observable<T> onMainThread(@NonNull Observable<T> observable) {
-        return observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        return onMainThread(observable, null);
+    }
+
+    public static <T> Observable<T> onMainThread(@NonNull Observable<T> observable, Scheduler scheduler) {
+        if (scheduler == null) {
+            scheduler = Schedulers.io();
+        }
+        return observable.subscribeOn(scheduler).observeOn(AndroidSchedulers.mainThread());
     }
 }
